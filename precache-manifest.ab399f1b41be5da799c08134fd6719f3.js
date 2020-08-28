996f9ae46e8718d5baccabb748b325499eddba22
Pranav Mahajan <mahajanpranav41@gmail.com>
2020-08-29 01:54:29 +0530

    Updates

---
+++ precache-manifest.ab399f1b41be5da799c08134fd6719f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c9ddd8cbfc356f7e799b74c947d941",
+    "url": "/newsai/index.html"
+  },
+  {
+    "revision": "d1019181f6e1ba8a76c7",
+    "url": "/newsai/static/css/main.bdb8ab58.chunk.css"
+  },
+  {
+    "revision": "b0d3f49e7a9872a6217d",
+    "url": "/newsai/static/js/2.9c1081d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newsai/static/js/2.9c1081d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1019181f6e1ba8a76c7",
+    "url": "/newsai/static/js/main.b3aab4b3.chunk.js"
+  },
+  {
+    "revision": "f2bd1ba5901a337d8a91",
+    "url": "/newsai/static/js/runtime-main.0d9a8abd.js"
+  }
+]);
